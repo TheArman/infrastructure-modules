f20ac78167c401221336fe8bc6fd1ed4ca86371b
thearman <arm.pro_v@mail.ru>
2024-08-09 17:09:05 +0400

    create nginx-controller module

---
+++ nginx-controller/main.tf
@@ -0,0 +1,15 @@
+module "nginx_controller" {
+  source = "terraform-iaac/nginx-controller/helm"
+  additional_set = [
+    {
+      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
+      value = "nlb"
+      type  = "string"
+    },
+    {
+      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-cross-zone-load-balancing-enabled"
+      value = "true"
+      type  = "string"
+    }
+  ]
+}
